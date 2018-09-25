b3b1f54c98b69c8b28a29b8ae92f33ba941494bb
Ella Väyrynen <ellajalkkis97@gmail.com>
2018-09-25 19:00:53 +0300

    some changes to the structure of the project on client side

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react"
+import Header from "./Layouts/Header"
+import Footer from "./Layouts/Footer"
+import Articles from "./Articles"
+
+export default class extends Component {
+    render() {
+        //Fragment is an outer wrapper instead of having a div.
+        return <Fragment>
+            <Header />
+            <Articles/>
+            <Footer />
+        </Fragment>
+    }
+}
---
+++ client/src/components/Articles/index.js
@@ -0,0 +1,4 @@
+import React from "react"
+
+export default props =>
+<div>Hello I am Articles folder</div>
